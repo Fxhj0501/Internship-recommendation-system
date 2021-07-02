a1c62d097e00d78c3e2288dc1776033a5523c361
Fxhj0501 <3037989509@qq.com>
2021-07-03 00:57:42 +0800

    finish login

--- Student/forLogin.py
+++ Student/forLogin.py
@@ -1,33 +1,34 @@
 from django.http import JsonResponse
 import json
 from common.models import student_info
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render
 
 # 登录处理
 def check_info( request):
     # 从 HTTP GET 请求中获取用户名、密码参数
-    username = request.POST.get('username')
-    password = request.POST.get('pwd')
-    sertype = request.POST.get('sertype')
-    response = {"usr":None,"msg":None}
-    valid_code = request.POST.get("vali")
-    user = authenticate(username=username,password = password)
-    if sertype == '学生':
-        if user:
-            login(request,user)
-            response["usr"] = user.username
-        else:
-            response['msg'] = "username or password error!"
-    else :
-        if user:
-            login(request,user)
-            response["usr"] = user.username
-        else:
-            response['msg'] = "username or password error!"
-
-    return JsonResponse(response)
-
+    if request.method == "POST":
+        username = request.POST.get("user")
+        pwd = request.POST.get("pwd")
+        response = {"usr":None,"msg":None,"pwd":None}
+       #user = authenticate(request,username=username,password = pwd)
+        # user = student_info.objects.get(username=username)
+        # if user.password == pwd:
+        #     login(request, user)  # request.user== 当前登录对象
+        #     response["usr"] = "duij"
+        #     return JsonResponse(response)
+        # else:
+        #     response["msg"] = '账号错了'
+        #     return JsonResponse(response)
+        try:
+            user = student_info.objects.get(username=username)
+        except:
+            response['msg'] = 'a'
+            return JsonResponse(response)
+        if user.password == pwd:
+            response['msg'] = 'b'
+            return JsonResponse(response)
 
 
 # 登出处理
@@ -35,3 +36,5 @@ def signout( request):
     # 使用登出方法
     logout(request)
     return JsonResponse({'ret': 0})
+
+
